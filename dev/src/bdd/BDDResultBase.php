4af21742bff516cc393d99d97ea598784fbb572b
adrien_lamoureux <vergil1534@gmail.com>
2014-10-29 12:06:31 +0100

    T12 : Lancement des BDD tests à index

--- dev/src/bdd/BDDResultBase.php
+++ dev/src/bdd/BDDResultBase.php
@@ -3,24 +3,24 @@
 include ('BDDResultPrinting.php');
 
 //Just interpret tests to send OK or KO
-class BDDResultBase implements BDDResultPrinting{
-    
+class BDDResultBase implements BDDResultPrinting {
+
     private $resList;
-    
+
     public function __construct() {
         $this->resList = new ArrayObject();
     }
-    
-    public function _setResult($resList){
+
+    public function _setResult($resList) {
         $this->resList = $resList;
     }
-    
-    public function _printResult(){
-        foreach($this->resList as $result){
-            if($result != TRUE){
-                die("Tests : KO<br />");
+
+    public function _printResult() {
+        foreach ($this->resList as $result) {
+            if ($result != TRUE) {
+                die("DataBase problem : Tests KO<br />");
             }
         }
-        echo("Tests : OK<br />");
     }
+
 }
