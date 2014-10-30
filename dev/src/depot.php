2e0cabf5a2483700451e5973c008c0f3f15f61b9
adrien_lamoureux <vergil1534@gmail.com>
2014-10-30 20:08:14 +0100

    T7 : Debug CV pour Git

--- dev/src/depot.php
+++ dev/src/depot.php
@@ -4,7 +4,7 @@ include("structure/haut_design.php");
 include("controleurs/controleur_depot.php");
 ?>
 
-<p> <a href=<?php $controleur->_printGitLink() ?>>Adresse du dépot Git</a></p>
+<p> <a href="<?php echo $controleur->_printGitLink() ?>">Adresse du dépot Git</a></p>
 
 <?php include("structure/bas_design.php"); ?>					
 						
