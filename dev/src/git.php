d26bb197f8d268d1fe2bbb1a5e07aadd471982d2
adrien_lamoureux <vergil1534@gmail.com>
2014-10-31 10:53:32 +0100

    US10 - T7 : Modification de l'icone de modification

--- dev/src/git.php
+++ dev/src/git.php
@@ -6,7 +6,7 @@ include("controleurs/controleur_git.php");
 <?php
 $controleur = new ControleurGit();
 ?>
-<p> <a href="<?php echo $controleur->_printGitLink() ?>">Adresse du dépot Git</a>
-    <a href="VueModifierGit.php?URL=<?php /*echo $controleur->_printGitLink()*/?>"> <img src="../resources/icone_modification.png"></a></p>
+<p> <a href="<?php echo $controleur->_printGitLink() ?>">Adresse du dépot Git </a>
+    <a href="VueModifierGit.php?URL=<?php /*echo $controleur->_printGitLink()*/?>" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-edit"></span></a></p>
 <?php include("structure/bas_design.php"); ?>					
 						
