1dcbc9e8dee94d6fe6959876de50c07b86eccba9
adrien_lamoureux <vergil1534@gmail.com>
2014-10-31 01:43:49 +0100

    T7 : Ajoute des New sur les fichiers

--- dev/src/c_modifierGit.php
+++ dev/src/c_modifierGit.php
@@ -1,10 +1,10 @@
 <?php
 include("structure/haut_design.php");
-//include("modeles/modele_git.php");
 include("controleurs/controleur_git.php");
 
 ?>
 <?php
+$controleur = new ControleurGit();
 $controleur->_modifyGitLink($_POST['NewURL']);
 ?>
 <?php include("structure/bas_design.php"); ?>					
--- dev/src/git.php
+++ dev/src/git.php
@@ -1,10 +1,12 @@
 ﻿<?php
 include("structure/haut_design.php"); 
-
 include("controleurs/controleur_git.php");
-$ControleurGit = new ControleurGit();
 ?>
 
-<p> <a href="<?php echo $ControleurGit->_printGitLink() ?>">Adresse du dépot Git</a></p>
+<?php
+$controleur = new ControleurGit();
+?>
+<p> <a href="<?php echo $controleur->_printGitLink() ?>">Adresse du dépot Git</a>
+    <a href="VueModifierGit.php?URL=<?php /*echo $controleur->_printGitLink()*/?>"> <img src="../resources/icone_modification.png"></a></p>
 <?php include("structure/bas_design.php"); ?>					
 						
