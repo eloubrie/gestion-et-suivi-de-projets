8a25b8e3e72d72457e147016a101d287091942e7
davidcheminade <cheminadedavid@gmail.com>
2014-11-04 21:09:31 +0100

    Correction du test de BDD qui ne passait plus

--- dev/src/bdd/BDDTestSQLBase.php
+++ dev/src/bdd/BDDTestSQLBase.php
@@ -11,7 +11,7 @@ class BDDTestSQLBase implements BDDTestSQL{
     
     function __construct() {
          $this->sqlSelect = "SELECT * FROM us";
-         $this->sqlInsert = "INSERT INTO `cdp`.`us` (`ID`, `titre`, `description`, `projet`, `sprint`, `cout`, `dependances`, `statut`, `date_debut`, `date_fin`, `date_test`, `ID_test`, `description_test`, `ID_git`) VALUES ('42', 'ddqd', 'dsqdsq', '8', '7', '6', 'ddsqdsqdsqd', '2', '2014-10-01', '2014-10-16', '2014-10-07', '7', 'zqdzqdqz', '2')";
+         $this->sqlInsert = "INSERT INTO `cdp`.`us` VALUES ('42', 'titre', 'desc', '8', '7', '6', 'dep', '2', '2014-10-01', '2014-10-16', '2014-10-07', 'codetest', 'descriptiontest', 'liengit')";
          $this->sqlRemove = "DELETE FROM `cdp`.`us` WHERE `us`.`ID` = 42";
          $this->sqlUpdate = "UPDATE `cdp`.`us` SET `titre` = 'ddqds' WHERE `us`.`ID` = 42";
          
