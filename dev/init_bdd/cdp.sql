265344c6bd0f6c1a0c0f1d19e631941ced94f5fe
adrien_lamoureux <vergil1534@gmail.com>
2014-11-05 14:56:02 +0100

    T18 : Revue de la table des sprints

--- dev/init_bdd/cdp.sql
+++ dev/init_bdd/cdp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 05 Novembre 2014 à 14:28
+-- Généré le :  Mer 05 Novembre 2014 à 14:55
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -60,24 +60,25 @@ INSERT INTO `git` (`ID`, `lien`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `sprints` (
-  `ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Numero_du_Sprint` int(11) NOT NULL,
   `Cout` int(11) NOT NULL,
   `Date_de_creation` date NOT NULL,
   `Date_de_debut` date NOT NULL,
-  `Duree` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Duree` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `Date_de_fin` date NOT NULL,
-  `Titre` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `Description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `Cout_valide` int(11) NOT NULL,
+  `Titre` text COLLATE utf8_unicode_ci NOT NULL,
+  `Description` text COLLATE utf8_unicode_ci NOT NULL,
+  `Cout_valide` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `sprints`
 --
 
-INSERT INTO `sprints` (`ID`, `Cout`, `Date_de_creation`, `Date_de_debut`, `Duree`, `Date_de_fin`, `Titre`, `Description`, `Cout_valide`) VALUES
-(1, 16, '2014-11-04', '2014-11-05', '3 mois', '2014-11-06', 'toto', 'totottotottoottoto', 10);
+INSERT INTO `sprints` (`ID`, `Numero_du_Sprint`, `Cout`, `Date_de_creation`, `Date_de_debut`, `Duree`, `Date_de_fin`, `Titre`, `Description`, `Cout_valide`) VALUES
+(1, 0, 16, '2014-11-04', '2014-11-05', '3 mois', '2014-11-06', 'toto', 'totottotottoottoto', 10);
 
 -- --------------------------------------------------------
 
@@ -115,27 +116,28 @@ INSERT INTO `taches` (`ID`, `titre`, `description`, `us`, `type`, `cout`, `depen
 
 CREATE TABLE IF NOT EXISTS `us` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `titre` text NOT NULL,
-  `description` text NOT NULL,
+  `titre` text COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
   `projet` int(11) NOT NULL,
-  `sprint` int(11) NOT NULL,
+  `ID_sprint` int(11) NOT NULL,
   `cout` int(11) NOT NULL,
-  `dependances` text NOT NULL,
+  `dependances` text COLLATE utf8_unicode_ci NOT NULL,
   `statut` int(11) NOT NULL,
   `date_debut` date NOT NULL,
   `date_fin` date NOT NULL,
   `date_test` date NOT NULL,
   `ID_test` int(11) NOT NULL,
-  `description_test` text NOT NULL,
+  `description_test` text COLLATE utf8_unicode_ci NOT NULL,
   `ID_git` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  PRIMARY KEY (`ID`),
+  KEY `ID_sprint` (`ID_sprint`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `us`
 --
 
-INSERT INTO `us` (`ID`, `titre`, `description`, `projet`, `sprint`, `cout`, `dependances`, `statut`, `date_debut`, `date_fin`, `date_test`, `ID_test`, `description_test`, `ID_git`) VALUES
+INSERT INTO `us` (`ID`, `titre`, `description`, `projet`, `ID_sprint`, `cout`, `dependances`, `statut`, `date_debut`, `date_fin`, `date_test`, `ID_test`, `description_test`, `ID_git`) VALUES
 (1, 'titre 1', 'Us numéro 1', 0, 1, 2, '0', 0, '2014-10-22', '2014-10-25', '0000-00-00', 5, 'Description du test 1', 0),
 (2, 'titre 2', 'US numéro 2', 0, 2, 5, '1', 2, '2014-10-30', '2014-10-31', '0000-00-00', 2, 'Description test numéro 2', 0),
 (5, 'ddqd', 'dsqdsq', 8, 7, 6, 'ddsqdsqdsqd', 2, '2014-10-01', '2014-10-16', '2014-10-07', 7, 'zqdzqdqz', 2),
