11614cb684ee6f8683138b1989f08bda218b49d3
davidcheminade <cheminadedavid@gmail.com>
2014-11-11 17:19:37 +0100

    BDD pour les tests

--- dev/init_bdd/cdp.sql
+++ dev/init_bdd/cdp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Lun 10 Novembre 2014 à 20:04
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Client: localhost
+-- Généré le: Mar 11 Novembre 2014 à 16:16
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `cdp`
+-- Base de données: `cdp`
 --
+CREATE DATABASE IF NOT EXISTS `cdp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `cdp`;
 
 -- --------------------------------------------------------
 
@@ -99,15 +101,16 @@ CREATE TABLE IF NOT EXISTS `sprints` (
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `cout_valide` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `sprints`
 --
 
 INSERT INTO `sprints` (`ID`, `numero_du_sprint`, `cout`, `date_creation`, `date_debut`, `duree`, `date_fin`, `titre`, `description`, `cout_valide`) VALUES
-(2, 1, 15, '2014-10-01', '0000-00-00', '15', '0000-00-00', 'sprint1', 'desc', 0),
-(3, 2, 14, '2014-10-14', '2014-10-15', '14', '2014-10-29', 'sprint2', 'desc', 14);
+(1, 1, 20, '2014-11-11', '2014-11-11', '10', '2014-11-21', 'Sprint 1', 'Description du sprint 1', 5),
+(2, 2, 18, '2014-11-11', '2014-11-21', '10', '2014-12-01', 'Sprint 2', 'Description du sprint 2', 0),
+(6, 3, 18, '2014-11-11', '2014-12-01', '5', '2014-12-06', 'Sprint 3', 'Description du sprint 3', 1);
 
 -- --------------------------------------------------------
 
@@ -129,17 +132,41 @@ CREATE TABLE IF NOT EXISTS `taches` (
   `date_test` date DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `ID_US` (`ID_US`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=42 ;
 
 --
 -- Contenu de la table `taches`
 --
 
 INSERT INTO `taches` (`ID`, `ID_US`, `titre`, `description`, `type`, `cout`, `dependances`, `developpeur`, `statut`, `date_realisation`, `date_test`) VALUES
-(1, 11, 'Tache 1', 'Description T1', 2, 3, '4', 1, 1, '0000-00-00', '0000-00-00'),
-(3, 10, 'creation BDD', 'Création de la Base de Donnée', 3, 2, '5', 1, 0, '0000-00-00', '0000-00-00'),
-(11, 11, 'mon Titre', 'jdkldkz', 1, 1, '2', 3, 0, '0000-00-00', '0000-00-00'),
-(13, 11, 'myTasce', 'my desce', 4, 987, '1', 2, 2, '2014-11-06', '1111-11-11');
+(14, 66, 'Tache 1', 'Desc1', 1, 3, '', 1, 0, '0000-00-00', '0000-00-00'),
+(15, 66, 'Tache 2', 'Desc2', 2, 1, '', 2, 2, '2014-11-26', '2014-11-27'),
+(16, 67, 'Tache 3', 'Desc3', 2, 3, '1, 2', 3, 0, '0000-00-00', '0000-00-00'),
+(17, 67, 'Tache 4', 'Desc4', 2, 6, '', 1, 0, '0000-00-00', '0000-00-00'),
+(18, 67, 'Tache 5', 'Desc5', 4, 3, '1, 2', 2, 0, '0000-00-00', '0000-00-00'),
+(19, 68, 'Tache 6', 'Desc6', 3, 2, '3', 1, 1, '0000-00-00', '0000-00-00'),
+(20, 68, 'Tache 7', 'Desc7', 3, 1, '4', 3, 1, '0000-00-00', '0000-00-00'),
+(21, 69, 'Tache 8', 'Desc8', 2, 7, '', 1, 0, '0000-00-00', '0000-00-00'),
+(22, 69, 'Tache 9', 'Desc9', 4, 2, '', 2, 2, '2014-11-26', '2014-11-28'),
+(23, 69, 'Tache 10', 'Desc10', 1, 9, '1, 5, 6', 3, 0, '0000-00-00', '0000-00-00'),
+(24, 70, 'Tache 11', 'Desc11', 2, 3, '', 3, 1, '0000-00-00', '0000-00-00'),
+(25, 70, 'Tache 12', 'Desc12', 4, 4, '5, 7', 1, 2, '2014-11-25', '2014-11-26'),
+(26, 70, 'Tache 13', 'Desc13', 1, 6, '', 3, 0, '0000-00-00', '0000-00-00'),
+(27, 70, 'Tache 14', 'Desc14', 1, 4, '', 2, 1, '0000-00-00', '0000-00-00'),
+(28, 71, 'Tache 15', 'Desc15', 2, 3, '1, 9', 1, 2, '2014-11-27', '0000-00-00'),
+(29, 71, 'Tache 16', 'Desc 16', 3, 2, '5, 12', 3, 1, '0000-00-00', '0000-00-00'),
+(30, 72, 'Tache 17', 'Desc 17', 1, 3, '', 2, 1, '0000-00-00', '0000-00-00'),
+(31, 72, 'Tache 18', 'Desc18', 2, 6, '8, 12', 3, 1, '0000-00-00', '0000-00-00'),
+(32, 72, 'Tache 19', 'Desc 19', 1, 4, '', 3, 0, '0000-00-00', '0000-00-00'),
+(33, 73, 'Tache 20', 'Desc 20', 4, 2, '', 2, 0, '0000-00-00', '0000-00-00'),
+(34, 73, 'Tache 21', 'Desc 21', 4, 7, '15', 1, 2, '2014-11-27', '0000-00-00'),
+(35, 73, 'Tache 22', 'Desc22', 1, 2, '', 1, 1, '0000-00-00', '0000-00-00'),
+(36, 73, 'Tache 23', 'Desc23', 2, 2, '', 2, 1, '0000-00-00', '0000-00-00'),
+(37, 74, 'Tache 24', 'Desc24', 1, 3, '20, 21', 2, 2, '2014-11-26', '2014-11-29'),
+(38, 74, 'Tache 25', 'Desc25', 3, 6, '8', 1, 1, '0000-00-00', '0000-00-00'),
+(39, 74, 'Tache 26', 'Desc26', 4, 1, '', 3, 0, '0000-00-00', '0000-00-00'),
+(40, 74, 'Tache 27', 'Desc27', 3, 1, '', 2, 1, '0000-00-00', '0000-00-00'),
+(41, 74, 'Tache 28', 'Desc 28', 2, 6, '14', 3, 2, '2014-11-19', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -162,21 +189,22 @@ CREATE TABLE IF NOT EXISTS `us` (
   `lien_git` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`ID`),
   KEY `ID_sprint` (`ID_sprint`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=66 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=75 ;
 
 --
 -- Contenu de la table `us`
 --
 
 INSERT INTO `us` (`ID`, `titre`, `description`, `ID_sprint`, `cout`, `statut`, `date_debut`, `date_fin`, `date_test`, `code_test`, `description_test`, `lien_git`) VALUES
-(10, 'Test', 'qsdqsd', 3, 610, 2, '1225-02-25', '1992-11-02', '0000-00-00', 'sfd', 'sdfsdf', 'lien'),
-(11, 'Test2', 'desc2', 3, 55, 2, '1992-11-02', '1311-11-11', '0000-00-00', 'code', 'desc', 'lien'),
-(13, 'Test de us4', 'desc4', 2, 987, 2, '4222-11-11', '4222-11-11', '0000-00-00', 'code4', 'desc4', 'lien4'),
-(27, 'ree', 'dedeD', 3, 1, 0, '0000-00-00', '0000-00-00', '0000-00-00', '$test = false', 'Test description v2', ''),
-(62, 'modify', 'de', 3, 1, 0, '0000-00-00', '0000-00-00', '0000-00-00', '', '', ''),
-(63, 'modify', 'toto', 3, 1, 0, '0000-00-00', '0000-00-00', '0000-00-00', '', '', ''),
-(64, 'toto', 'tata', 2, 1, 0, '1992-02-25', '1992-02-26', '0000-00-00', '$test = true', 'a test', 'https://lol?'),
-(65, 'TestTest', 'a test', 2, 1, 0, '0000-00-00', '0000-00-00', '0000-00-00', '$test = true', 'Test description', '');
+(66, 'Titre US1', 'Description US1', 1, 5, 1, '2014-11-11', '2014-11-13', '2014-11-13', 'Code US1', 'Desc Test US1', 'Lien git US1'),
+(67, 'Titre US2', 'Description US2', 1, 7, 0, '2014-11-15', '2014-11-17', '0000-00-00', 'Code US2', 'Desc Test US2', 'Lien US2'),
+(68, 'Titre US3', 'Description US3', 2, 7, 0, '2014-11-20', '2014-11-21', '0000-00-00', 'code US3', 'Desc test US3', 'Lien US3'),
+(69, 'Titre US4', 'Description US4', 2, 1, 2, '2014-11-19', '2014-11-22', '2014-11-27', 'Code US4', 'Desc US4', 'Lien US4'),
+(70, 'Titre US5', 'Desc US5', 2, 3, 0, '2014-11-19', '2014-11-21', '0000-00-00', 'Code US5', 'Desc US5', 'Lien US5'),
+(71, 'Titre US6', 'Desc US6', 6, 2, 0, '2014-11-29', '2014-11-30', '0000-00-00', 'Code US6', 'Desc US6', 'Lien US6'),
+(72, 'Titre US7', 'Desc US7', 6, 5, 3, '2014-11-20', '2014-11-22', '2014-11-27', 'Code US7', 'Desc US7', 'Lien US7'),
+(73, 'Titre US8', 'Desc US8', 6, 3, 1, '2014-11-25', '2014-11-28', '0000-00-00', 'Code US8', 'Desc US8', 'Lien US8'),
+(74, 'Titre US9', 'Desc US9', 6, 4, 0, '2014-11-25', '2014-11-29', '0000-00-00', 'Code US9', 'Desc US9', 'Lien US9');
 
 --
 -- Contraintes pour les tables exportées
