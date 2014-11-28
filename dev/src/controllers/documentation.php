058c8a76f675fbb0d49f4a86fda33e19e1e89437
davidcheminade <cheminadedavid@gmail.com>
2014-11-28 18:02:42 +0100

    Petites correction docs + menu

--- dev/src/controllers/documentation.php
+++ dev/src/controllers/documentation.php
@@ -22,32 +22,36 @@ class ControllerDocumentation
         
         private function _buildDevelopersList($developersList)
         {
-                foreach ($developersList as $developer) {
-                        echo $developer['pseudo'].' - ';
+				$a = 0;
+                foreach ($developersList as $developer) 
+				{
+						if($a>0)
+						{ echo ' - '; }
+                        echo $developer['pseudo'];
+						$a = 1;
                 }
         }
     
         public function _buildDocumentationInfo($id)
-	{
-		$documentation = $this->modelDocumentation->_getDocumentation($id)->fetch(PDO::FETCH_ASSOC);
-                $devs = $this->modelDev->_getDevelopers();
-                $git = $this->modelGit->_getGitURL()->fetch(PDO::FETCH_ASSOC);
-		?>
-                <h3 style="text-decoration: underline;">Informations générales sur le projet :</h3><br />
-		<p><b>Description du projet :</b> <?php echo $documentation['description']; ?></p>
-                <p><b>Développeurs participant au projet :</b> <?php $this->_buildDevelopersList($devs); ?></p>
-		<br />
-                
-                <h3 style="text-decoration: underline;">Informations techniques sur le projet :</h3><br />
-                <p><b>Dépôt des sources du projet :</b> <a href="<?php echo $git['lien']; ?>"><?php echo $git['lien']; ?></a></p>
-                <p><b>Systèmes d'exploitations supportés :</b> <?php echo $documentation['systemes_exploitation']; ?></p>
-                <p><b>Navigateurs supportés :</b> <?php echo $documentation['navigateurs']; ?></p>
-                <p><b>Langages de programmations utilisés :</b> <?php echo $documentation['langages']; ?></p>
-                <p><b>Frameworks utilisés :</b> <?php echo $documentation['frameworks']; ?></p>
-                <p><b>Autres outils utilisés :</b> <?php echo $documentation['autres_outils']; ?></p>
-                
-		<?php
-	}
+		{
+			$documentation = $this->modelDocumentation->_getDocumentation($id)->fetch(PDO::FETCH_ASSOC);
+			$devs = $this->modelDev->_getDevelopers();
+			$git = $this->modelGit->_getGitURL()->fetch(PDO::FETCH_ASSOC);
+			?>
+			<h3>Informations générales sur le projet :</h3><br />
+			<p><b>Description du projet :</b> <?php echo $documentation['description']; ?></p>
+			<p><b>Développeurs participant au projet :</b> <?php $this->_buildDevelopersList($devs); ?></p>
+			<br />
+			
+			<h3>Informations techniques sur le projet :</h3><br />
+			<p><b>Dépôt des sources du projet :</b> <a href="<?php echo $git['lien']; ?>"><?php echo $git['lien']; ?></a></p>
+			<p><b>Systèmes d'exploitations supportés :</b> <?php echo $documentation['systemes_exploitation']; ?></p>
+			<p><b>Navigateurs supportés :</b> <?php echo $documentation['navigateurs']; ?></p>
+			<p><b>Langages de programmations utilisés :</b> <?php echo $documentation['langages']; ?></p>
+			<p><b>Frameworks utilisés :</b> <?php echo $documentation['frameworks']; ?></p>
+			<p><b>Autres outils utilisés :</b> <?php echo $documentation['autres_outils']; ?></p>
+			<?php
+		}
         
         public function _editDocumentation($description, $operatingSystems, $navigators, $languages, $frameworks, $otherTools)
         {
